477c06653884598dcacd846fdc50a6d521613a8f
WIjaksara Aptaluhung <19623024@std.stei.itb.ac.id>
2025-10-27 20:38:44 +0700

    refactor: env buat deploy

--- src/config.php
+++ src/config.php
@@ -1,10 +1,11 @@
 <?php
-$host = 'db';
-$user = 'user';
-$pass = 'password';
-$dbname = 'kimiapp';
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$db   = getenv('DB_NAME');
+$port = getenv('DB_PORT');
 
-$conn = new mysqli($host, $user, $pass, $dbname);
+$conn = new mysqli($host, $user, $pass, $db, $port);
 
 if ($conn->connect_error) {
   die("Koneksi gagal: " . $conn->connect_error);
