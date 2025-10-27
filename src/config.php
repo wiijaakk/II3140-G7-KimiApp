51f2c357003449c0186caea1024b8c2d18db90cf
WIjaksara Aptaluhung <19623024@std.stei.itb.ac.id>
2025-10-27 22:42:07 +0700

    refactor: masih bergelut dengan env

--- src/config.php
+++ src/config.php
@@ -1,9 +1,9 @@
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER');
-$pass = getenv('DB_PASS');
-$db   = getenv('DB_NAME');
-$port = getenv('DB_PORT');
+$host = getenv('MYSQLHOST');
+$user = getenv('MYSQLUSER');
+$pass = getenv('MYSQLPASSWORD');
+$db   = getenv('MYSQL_DATABASE');
+$port = getenv('MYSQLPORT');
 
 $conn = new mysqli($host, $user, $pass, $db, $port);
 
