96e6712e903fc0d8a11bf975da5a58156ffc6955
WIjaksara Aptaluhung <19623024@std.stei.itb.ac.id>
2025-11-06 20:58:58 +0700

    refactor: ini edit buar profile bisa liat progres

--- src/profile.js
+++ src/profile.js
@@ -15,6 +15,7 @@ document.addEventListener('DOMContentLoaded', function() {
         
         if (data.ok && data.completedSections) {
             openedSections = new Set(data.completedSections);
+            print(openedSections);
             updateProgress();
         }
         } catch (error) {
